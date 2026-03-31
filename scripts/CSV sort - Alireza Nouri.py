952c49175ca03a672dd75ff07687e59e5d817585
Azerila1 <102166488+Azerila1@users.noreply.github.com>
2026-03-31 13:59:30 -0400

    Update CSV sort - Alireza Nouri.py

--- scripts/CSV sort - Alireza Nouri.py
+++ scripts/CSV sort - Alireza Nouri.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
-import os
-FilteredColumn = pd.read_csv(r'C:\Users\salir\Downloads\MAIN DATA.csv', encoding ='latin1')
+FilteredColumn = pd.read_csv(r'C:\Users\salir\Downloads\MAIN DATA.csv', encoding='cp1252')
+
 FilteredColumn = FilteredColumn[['DGUID', 'GEO_LEVEL', 'CENSUS_YEAR', 'GEO_NAME', 'TOTAL_GENDER (1)', 'CHARACTERISTIC_NAME', 'AGE']]
 FilteredColumn = FilteredColumn[
     (
         (FilteredColumn['GEO_LEVEL'] == "Census subdivision") |
         (FilteredColumn['GEO_LEVEL'] == "Provincial/territorial combined CSDs with 2021 population under 10,000")
     ) &
-    (FilteredColumn['AGE'] == "Total - Age") &
     (
         (FilteredColumn['CHARACTERISTIC_NAME'] == "  0 to 14 years") |
         (FilteredColumn['CHARACTERISTIC_NAME'] == "  15 to 64 years") |
@@ -17,6 +16,12 @@ FilteredColumn = FilteredColumn[
 ]
 
 FilteredColumn = FilteredColumn.drop_duplicates()
-FilteredColumn.to_csv('test.csv', index=False)
 
-print(os.getcwd())
+FilteredColumn = FilteredColumn.pivot_table(
+    index=['DGUID', 'GEO_LEVEL', 'CENSUS_YEAR', 'GEO_NAME'],
+    columns='CHARACTERISTIC_NAME',
+    values='TOTAL_GENDER (1)',
+    aggfunc='first'
+).reset_index()
+
+FilteredColumn.to_csv('test.csv', index=False, encoding='utf-8-sig')
